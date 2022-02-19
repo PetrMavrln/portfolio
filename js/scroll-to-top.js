69163a276f5bc8d7ad3cb6d93225d0f34b2e0c10
Petr Mavrin <petr.mavrin@gmail.com>
2022-02-20 00:43:43 +0400

    tweak scroll btn + businessCard

--- js/scroll-to-top.js
+++ js/scroll-to-top.js
@@ -5,10 +5,10 @@ function trackScroll() {
     let scrolled = window.pageYOffset;
     let coords = document.documentElement.clientHeight;
 
-    if (scrolled > coords) {
+    if (scrolled > coords / 2) {
         scrollToTopBtn.classList.add("scroll-to-top-show");
     }
-    if (scrolled < coords) {
+    if (scrolled < coords / 2) {
         scrollToTopBtn.classList.remove("scroll-to-top-show");
     }
 }
@@ -21,8 +21,32 @@ function scrollToTop() {
     }
 }
 
+function scrollOpacity() {
+    let scrolled = window.pageYOffset;
+    let coords = document.documentElement.clientHeight;
+    let opacity = 0.1;
+
+    if (scrolled > coords / 2) {
+        scrollToTopBtn.style.opacity = opacity;
+    }
+
+    if (scrolled > coords / 1.7) {
+        scrollToTopBtn.style.opacity = opacity + 0.2;
+    }
+
+    if (scrolled > coords / 1.4) {
+        scrollToTopBtn.style.opacity = opacity + 0.3;
+    }
+
+    if (scrolled > coords) {
+        scrollToTopBtn.style.opacity = opacity + 0.4;
+    }
+
+}
+
 let scrollToTopBtn = document.querySelector(".scroll-to-top");
 
 window.addEventListener("scroll", trackScroll);
+window.addEventListener("scroll", scrollOpacity);
 scrollToTopBtn.addEventListener("click", scrollToTop);
 })();
--- projects/businessCard/script.js
+++ projects/businessCard/script.js
@@ -1,5 +1,5 @@
 let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
-themeButton.onclick = function() {
+themeButton.addEventListener('click', function() {
     page.classList.toggle('dark-theme');
-};
+});
